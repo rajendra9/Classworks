21f38fa0491e41bb6d094c358d05a7f6e4c23b17
HTC-AP-Hadoop-Training <36527618+HTC-AP-Hadoop-Training@users.noreply.github.com>
2018-04-18 08:30:00 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,121 @@
+package code;
+
+public class Employee {
+
+	private int empno;
+	private String firstName;
+	private String lastName;
+	private double salary;
+	private String designation;
+	
+	public Employee() {}
+
+	public Employee(int empno, String firstName, String lastName, double salary, String designation) {
+		super();
+		this.empno = empno;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.designation = designation;
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName() {
+		return getFirstName() + " " + getLastName();
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((designation == null) ? 0 : designation.hashCode());
+		result = prime * result + empno;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (designation == null) {
+			if (other.designation != null)
+				return false;
+		} else if (!designation.equals(other.designation))
+			return false;
+		if (empno != other.empno)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ ", designation=" + designation + "]";
+	}
+
+	
+	
+}
+
